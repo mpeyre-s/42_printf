3f765375e772f87b5ec241db904dc895a09a2cd4
mpeyre-s <mathispeyre0@gmail.com>
2024-11-20 12:03:57 +0100

    v0.61

--- printhex.c
+++ printhex.c
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:58:52 by mathispeyre       #+#    #+#             */
-/*   Updated: 2024/11/20 11:58:15 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2024/11/20 12:03:31 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,6 @@ static int	hexlen(unsigned int nb)
 	int	result;
 
 	result = 0;
-	if (nb < 0)
-	{
-		result++;
-		nb = -nb;
-	}
 	while (nb > 15)
 	{
 		result += hexlen(nb / 16);
@@ -36,11 +31,6 @@ void	print_hexlower(unsigned int nb)
 	char	*hex_base;
 
 	hex_base = "0123456789abcdef";
-	if (nb < 0)
-	{
-		nb = -nb;
-		ft_putchar_fd('-', 1);
-	}
 	while (nb > 15)
 		print_hexlower(nb / 16);
 	c = hex_base[nb % 16];
@@ -53,11 +43,6 @@ void	print_hexupper(unsigned int nb)
 	char	*hex_base;
 
 	hex_base = "0123456789ABCDEF";
-	if (nb < 0)
-	{
-		nb = -nb;
-		ft_putchar_fd('-', 1);
-	}
 	while (nb > 15)
 		print_hexupper(nb / 16);
 	c = hex_base[nb % 16];
