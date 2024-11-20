e14c85997b745492d63a2034e1b0713ddd359132
mpeyre-s <mathispeyre0@gmail.com>
2024-11-20 12:38:49 +0100

    v0.9

--- printhex.c
+++ printhex.c
@@ -6,7 +6,7 @@
 /*   By: mathispeyre <mathispeyre@student.42.fr>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/15 11:58:52 by mathispeyre       #+#    #+#             */
-/*   Updated: 2024/11/20 12:03:31 by mathispeyre      ###   ########.fr       */
+/*   Updated: 2024/11/20 12:38:22 by mathispeyre      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,40 @@ void	print_hexlower(unsigned int nb)
 {
 	char	c;
 	char	*hex_base;
+	char	buffer[9];
+	int		i;
 
 	hex_base = "0123456789abcdef";
+	i = 8;
+	buffer[i] = '\0';
 	while (nb > 15)
-		print_hexlower(nb / 16);
-	c = hex_base[nb % 16];
-	ft_putchar_fd(c, 1);
+	{
+		buffer[--i] = hex_base[nb % 16];
+		nb /= 16;
+	}
+	buffer[--i] = hex_base[nb % 16];
+	while (buffer[i])
+		ft_putchar_fd(buffer[i++], 1);
 }
 
 void	print_hexupper(unsigned int nb)
 {
 	char	c;
 	char	*hex_base;
+	char	buffer[9];
+	int		i;
 
 	hex_base = "0123456789ABCDEF";
+	i = 8;
+	buffer[i] = '\0';
 	while (nb > 15)
-		print_hexupper(nb / 16);
-	c = hex_base[nb % 16];
-	ft_putchar_fd(c, 1);
+	{
+		buffer[--i] = hex_base[nb % 16];
+		nb /= 16;
+	}
+	buffer[--i] = hex_base[nb % 16];
+	while (buffer[i])
+		ft_putchar_fd(buffer[i++], 1);
 }
 
 int	hexhub(unsigned int nb, char type)
